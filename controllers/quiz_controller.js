f3039cb110c99fda59a89785dd3546c2f0c0778c
Jotape68 <juanpedro.moreno@havasmedia.com>
2015-06-13 12:19:08 +0200

    Busqueda preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,8 +14,8 @@ exports.load = function(req, res, next, quizId) {
 
 
 // GET /quizes
-exports.index = function(req, res) {
-  models.Quiz.findAll().then(function(quizes) {
+exports.index = function(req, res, busq) {
+  models.Quiz.findAll({where: {"pregunta": {like: req.query.search}}}).then(function(quizes) {
     res.render('quizes/index', { quizes: quizes});
   }
  ).catch(function(error) {next(error);})
